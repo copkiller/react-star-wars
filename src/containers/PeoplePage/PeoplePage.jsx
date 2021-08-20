0020b12a698d78751353c04331f3933c54a6eb6d
copkiller <semyonlinkov@gmail.com>
2021-08-20 16:53:59 +0400

    prop-types

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
 import { withErrorApi } from '@hoc-helpers/withErrorApi';
-import { getApiResource } from '@utils/network';
-import { API_PEOPLE } from '@constants/api';
-import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
 import PeopleList from '@components/PeoplePage/PeopleList';
+import { getApiResource } from '@utils/network';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
+import { API_PEOPLE } from '@constants/api';
 
 import styles from './PeoplePage.module.css';
 
@@ -43,6 +45,10 @@ const PeoplePage = ({ setErrorApi }) => {
          {people && <PeopleList people={people} />}
       </>
    );
+};
+
+PeoplePage.propTypes = {
+   setErrorApi: PropTypes.func,
 };
 
 export default withErrorApi(PeoplePage);
